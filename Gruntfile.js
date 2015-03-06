a810bc2558c47b51de0353ed874ecc0ea7269445
James McKay <mckayj@parliament.uk>
2015-03-06 16:42:28 +0000

    Add grunt task to create a require.js config file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,11 @@ module.exports = function(grunt) {
                 }
             }
         },
+        bowerRequirejs: {
+            target: {
+                rjsConfig: 'src/assets/js/main.js'
+            }
+        },
         watch: {
             files: ['src/less/*.less', 'src/less/**/*.less'],
             tasks: ['less']
@@ -15,5 +20,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-requirejs');
     grunt.registerTask('default', ['watch']);
 };
---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    jquery: "../../../bower_components/jquery/dist/jquery",
+    requirejs: "../../../bower_components/requirejs/require"
+  },
+  packages: [
+
+  ]
+});
