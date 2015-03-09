ad580c8eb9e294184209680bae2acaa4d1c4cca2
James McKay <mckayj@parliament.uk>
2015-03-09 15:48:25 +0000

    Load the taxonomy from the Elda endpoint.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,28 @@
-define(['jquery'], function($) {
+define(['jquery', 'ddp'], function($, ddp) {
+
+    var terms;
+
+    (function() {
+        var loading = document.getElementById('loading');
+        var progressValue = document.getElementById('progress-value');
+        var progressMeter = document.getElementById('progress-meter');
+
+        $(loading).show();
+
+        ddp.getTerms()
+            .progress(function(state) {
+                console.log(state);
+                var percentage = Math.round(state.loaded * 100 / state.expected);
+                progressValue.innerHTML = percentage + '%';
+                progressMeter.max = state.expected;
+                progressMeter.min = 0;
+                progressMeter.value = state.loaded;
+            })
+            .done(function(result) {
+                console.log(result);
+                terms = result;
+                $(loading).hide();
+            });
+    })();
 
 });
---
+++ src/assets/js/ddp.js
@@ -0,0 +1,68 @@
+define(['jquery'], function($) {
+    /**
+     * The base URL for the DDP ELDA endpoint
+     */
+    var ddpBase = 'http://lda.data.parliament.uk/'
+    var termClass = 'ID';
+    var termPageSize = 383;
+
+    /* ====== loadTopics ====== */
+
+    var terms = false;
+
+    function getTerms(doneCallback, progressCallback) {
+
+        var deferred = $.Deferred();
+
+        if (!progressCallback) progressCallback = function() {};
+        if (!doneCallback) doneCallback = function() {};
+
+        function loadTerms(page) {
+            var url = ddpBase
+                + 'terms.json?_pageSize=' + termPageSize
+                + '&_page=' + page
+                + '&class=' + termClass;
+            $.ajax({
+                url: url,
+                dataType: 'json',
+                success: function(data) {
+                    terms = terms.concat(data.result.items);
+                    console.log(data);
+                    var maxPage = data.result.totalResults / data.result.itemsPerPage;
+                    page++;
+
+                    var state = {
+                        loaded: terms.length,
+                        expected: data.result.totalResults,
+                    };
+
+                    progressCallback(state);
+                    deferred.notify(state);
+
+                    if (page < maxPage) {
+                        loadTerms(page);
+                    }
+                    else {
+                        doneCallback(terms);
+                        deferred.resolve(terms);
+                    }
+                }
+            });
+        }
+
+        if (terms !== false) {
+            doneCallback(terms);
+            deferred.resolve(terms);
+        }
+        else {
+            terms = [];
+            loadTerms(0);
+        }
+
+        return deferred.promise();
+    }
+
+    return {
+        getTerms: getTerms
+    };
+});
