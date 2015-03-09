09cbc2c4faa4173a51e7d620031efa5e8e4c8a96
James McKay <mckayj@parliament.uk>
2015-03-09 16:32:14 +0000

    Add knockout.js and Selectize

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,9 @@ require.config({
   },
   paths: {
     jquery: "../lib/jquery/dist/jquery",
-    requirejs: "../lib/requirejs/require"
+    requirejs: "../lib/requirejs/require",
+    knockout: "../lib/knockout/dist/knockout",
+    selectize: "../lib/selectize/dist/js/selectize"
   },
   packages: [
 
