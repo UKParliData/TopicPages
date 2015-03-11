d8231cffa122c73f3ad91912b9fc4e28188e5d24
James McKay <mckayj@parliament.uk>
2015-03-11 15:39:18 +0000

    Set up selectize for the taxonomy finder

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,11 @@
 require.config({
   shim: {
-
+    'selectize' : {
+      deps: [
+        'css!../lib/selectize/dist/css/selectize.css',
+        'css!../lib/selectize/dist/css/selectize.default.css'
+      ]
+    }
   },
   paths: {
     jquery: "../lib/jquery/dist/jquery",
@@ -9,7 +14,12 @@ require.config({
     selectize: "../lib/selectize/dist/js/selectize",
     text: "../lib/text/text",
     toastr: "../lib/toastr/toastr",
-    "es5-shim": "../lib/es5-shim/es5-shim"
+    "es5-shim": "../lib/es5-shim/es5-shim",
+    microplugin: "../lib/microplugin/src/microplugin",
+    sifter: "../lib/sifter/sifter",
+    css: "../lib/require-css/css",
+    "css-builder": "../lib/require-css/css-builder",
+    normalize: "../lib/require-css/normalize"
   },
   packages: [
 
@@ -19,13 +29,37 @@ require.config({
 define([
     'jquery',
     'knockout',
-    'navigator'
+    'navigator',
+    'selectize'
 ], function($, ko, navigator) {
     "use strict";
-
     $.support.cors = true;
+
+    /* ===== Components ====== */
+
     ko.components.register('loader', { require: 'modules/loader' });
     ko.components.register('home', { require: 'modules/home' });
     ko.components.register('error', { template: { require: 'text!../templates/error.html' } });
     ko.applyBindings(navigator);
+
+    ko.bindingHandlers.selectize = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var options = {
+                options: ko.unwrap(allBindingsAccessor.get("options")),
+                labelField: ko.unwrap(allBindingsAccessor.get("optionsText")),
+                valueField: ko.unwrap(allBindingsAccessor.get("optionsValue")),
+                searchField: ko.unwrap(allBindingsAccessor.get("optionsText")),
+                sortField: ko.unwrap(allBindingsAccessor.get("optionsText"))
+            };
+            var $select = $(element).selectize(options);
+        },
+        update: function (element, valueAccessor, allBindingsAccessor) {
+            var options = ko.unwrap(allBindingsAccessor.get("options"));
+            if (options.length > 0) {
+                for (var i = 0; i < options.length; i++)
+                    $(element).selectize()[0].selectize.addOption(options[i]);
+                $(element).selectize()[0].selectize.enable();
+            }
+        }
+    }
 });
--- src/assets/js/modules/home.js
+++ src/assets/js/modules/home.js
@@ -1,10 +1,20 @@
 define([
-    'text!../../templates/home.html'
-], function(tpl) {
+    'text!../../templates/home.html',
+    'ddp',
+    'knockout',
+    'selectize'
+], function(tpl, ddp, ko) {
     "use strict";
+
+    var viewModel = function() {
+        var self = this;
+
+        self.topics = ko.observableArray(ddp.getTerms());
+        self.selectedTopic = ko.observable(null);
+    }
 
     return {
         template: tpl,
-        viewModel: function() {}
+        viewModel: viewModel
     };
 });
