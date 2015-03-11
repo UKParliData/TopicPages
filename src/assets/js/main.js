35f997b3402b4a63e0dd9445844ebf12c7bac62d
James McKay <mckayj@parliament.uk>
2015-03-11 15:41:12 +0000

    Move initialisation code back to a separate init.js file.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,40 +26,4 @@ require.config({
   ]
 });
 
-define([
-    'jquery',
-    'knockout',
-    'navigator',
-    'selectize'
-], function($, ko, navigator) {
-    "use strict";
-    $.support.cors = true;
-
-    /* ===== Components ====== */
-
-    ko.components.register('loader', { require: 'modules/loader' });
-    ko.components.register('home', { require: 'modules/home' });
-    ko.components.register('error', { template: { require: 'text!../templates/error.html' } });
-    ko.applyBindings(navigator);
-
-    ko.bindingHandlers.selectize = {
-        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var options = {
-                options: ko.unwrap(allBindingsAccessor.get("options")),
-                labelField: ko.unwrap(allBindingsAccessor.get("optionsText")),
-                valueField: ko.unwrap(allBindingsAccessor.get("optionsValue")),
-                searchField: ko.unwrap(allBindingsAccessor.get("optionsText")),
-                sortField: ko.unwrap(allBindingsAccessor.get("optionsText"))
-            };
-            var $select = $(element).selectize(options);
-        },
-        update: function (element, valueAccessor, allBindingsAccessor) {
-            var options = ko.unwrap(allBindingsAccessor.get("options"));
-            if (options.length > 0) {
-                for (var i = 0; i < options.length; i++)
-                    $(element).selectize()[0].selectize.addOption(options[i]);
-                $(element).selectize()[0].selectize.enable();
-            }
-        }
-    }
-});
+require(['init']);
