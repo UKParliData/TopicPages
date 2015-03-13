b796ade553f48e0cec08b979a2b367d8898ebeb9
James McKay <mckayj@parliament.uk>
2015-03-13 16:04:32 +0000

    Remove dropdown from home module.
    It isn't needed now that it's being rendered in the main window.

--- src/assets/js/modules/home.js
+++ src/assets/js/modules/home.js
@@ -1,30 +1,9 @@
 define([
-    'text!../../templates/home.html',
-    'ddp',
-    'knockout',
-    'navigator',
-    'selectize'
-], function(tpl, ddp, ko, nav) {
+    'text!../../templates/home.html'
+], function(tpl) {
     "use strict";
 
-    var viewModel = function() {
-        var self = this;
-
-        self.topics = ko.observableArray(ddp.getTerms());
-        self.selectedTopic = ko.observable(null);
-
-        self.showPage = function(a) {
-            nav.navigateTo(a.target, self.selectedTopic());
-        };
-
-        self.pages = [
-            { pageTitle: 'Briefing Papers', target: 'briefing-papers' },
-            { pageTitle: 'Early Day Motions', target: 'edms' }
-        ];
-    }
-
     return {
-        template: tpl,
-        viewModel: viewModel
+        template: tpl
     };
 });
