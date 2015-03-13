4123c8548621322cfcca7d84dedff5a4c277f96d
James McKay <mckayj@parliament.uk>
2015-03-13 15:06:11 +0000

    Add rendering for early day motions.

--- src/assets/js/modules/edms.js
+++ src/assets/js/modules/edms.js
@@ -1,14 +1,62 @@
 define([
-    'text!../../templates/edms.html',
+    'jquery',
+    'knockout',
     'navigator',
-    'config'
-], function(tpl, nav, cfg) {
+    'config',
+    'text!../../templates/edms.html',
+    'utils'
+], function($, ko, nav, cfg, tpl) {
     "use strict";
 
+    function loadEdm(item) {
+        console.log(item);
+        return {
+            uri: item._about,
+            date: new Date(item.dateTabled._value),
+            motionText: item.motionText,
+            externalUrl: item.externalLocation,
+            edmNumber: item.edmNumber._value,
+            title: item.title,
+            sponsors: item.sponsors.map(function(s, ix) {
+                return {
+                    uri: s._about,
+                    name: item.sponsorPrinted[ix]
+                };
+            }),
+            topics: item.topic,
+            type: item.type
+        }
+    }
+
+    var EdmViewModel = function(termUri) {
+        var self = this;
+
+        self.loading = ko.observable(false);
+        self.edms = ko.observableArray([]);
+
+        var load = function(page) {
+            var url = cfg.edms.queryByTopic.format(page, termUri);
+            self.loading(true);
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json'
+            }).done(function(data) {
+                var result = data.result;
+                var items = result.items;
+                var edms = items.map(loadEdm);
+                self.loading(false);
+                self.edms(edms);
+                console.log(edms);
+            });
+        };
+
+        load(0);
+    }
 
     var viewModel = function() {
-        // TODO: replace this
-        console.log(nav.parameters());
+        var result = new EdmViewModel(nav.parameters());
+        return result;
     };
 
     return {
