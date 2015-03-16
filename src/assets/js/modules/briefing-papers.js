acdbe115ac511937ff182d2adda5aefc1035429a
James McKay <mckayj@parliament.uk>
2015-03-16 15:49:49 +0000

    Refactor briefing papers view model to use base class.

--- src/assets/js/modules/briefing-papers.js
+++ src/assets/js/modules/briefing-papers.js
@@ -1,63 +1,39 @@
 define([
-    'jquery',
-    'knockout',
-    'navigator',
-    'config',
+    'modules/module',
     'text!../../templates/briefing-papers.html',
     'utils'
-], function($, ko, nav, cfg, tpl) {
+], function(module, tpl) {
     "use strict";
 
-    function loadBriefingPaper(item) {
-        return {
-            uri: item._about,
-            date: new Date(item.date._value),
-            description: item.description[0],
-            identifier: item.identifier._value,
-            title: item.title,
-            topics: item.topic.map(function(t) {
-                return {
-                    uri: t._about,
-                    title: t.prefLabel._value
-                };
-            }),
-            type: item.type
-        }
-    }
-
-    var BriefingPaperViewModel = function(topic) {
+    function BriefingPaperViewModel() {
         var self = this;
+        module.ModuleViewModel.call(self, 'briefingPapers');
 
-        self.loading = ko.observable(false);
-        self.papers = ko.observableArray([]);
+        /* ====== Overridden methods ====== */
 
-        var load = function(page) {
-            var url = cfg.briefingPapers.queryByTopic.format(page, topic);
-            self.loading(true);
-            $.ajax({
-                url: url,
-                method: 'GET',
-                dataType: 'json'
-            }).done(function(data) {
-                var result = data.result;
-                var items = result.items;
-                var briefingPapers = items.map(loadBriefingPaper);
-                self.loading(false);
-                self.papers(briefingPapers);
-                console.log(briefingPapers);
-            });
+        self.loadItem = function(item) {
+            console.log(item);
+            return {
+                uri: item._about,
+                date: new Date(item.date._value),
+                description: item.description[0],
+                identifier: item.identifier._value,
+                title: item.title,
+                topics: item.topic.map(function(t) {
+                    return {
+                        uri: t._about,
+                        title: t.prefLabel._value
+                    };
+                }),
+                type: item.type
+            }
         };
 
-        load(0);
+        self.load(0);
     }
-
-    var viewModel = function() {
-        var result = new BriefingPaperViewModel(nav.selectedTopic());
-        return result;
-    };
 
     return {
         template: tpl,
-        viewModel: viewModel
+        viewModel: BriefingPaperViewModel
     };
 });
