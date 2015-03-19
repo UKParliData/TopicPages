fcf3f62bf84ad70cd0f07107b85380f5f85a309a
James McKay <mckayj@parliament.uk>
2015-03-19 16:58:15 +0000

    Discard legacy module view model no longer in use.

--- src/assets/js/modules/module.js
+++ src/assets/js/modules/module.js
@@ -1,52 +1,15 @@
 define([
     'jquery',
     'knockout',
-    'config',
     'navigator',
-    'loader',
-    'utils'
-], function($, ko, cfg, nav, loader) {
+    'loader'
+], function($, ko, nav, loader) {
 
     function _abstract() {
         throw 'This method has not been defined in the derived class.';
     }
 
     /* ====== Module class definition ====== */
-
-    function LegacyModuleViewModel(configName) {
-        var self = this;
-        var config = cfg.modules[configName];
-        var topic = nav.selectedTopic();
-
-        /* ====== Observables ====== */
-
-        self.items = ko.observableArray([]);
-
-        /* ====== Public methods ====== */
-
-        self.load = function(page) {
-            var url = config.queryByTopic;
-            url += url.indexOf('?') >= 0 ? '&' : '?';
-            url += '_page={0}&topic={1}'.format(page, topic);
-            nav.componentLoading(true);
-            $.ajax({
-                url: url,
-                method: 'GET',
-                dataType: 'json'
-            }).done(function(data) {
-                var result = data.result;
-                var items = result.items.map(self.loadItem);
-                nav.componentLoading(false);
-                self.items(items);
-            });
-        }
-
-        /* ====== Overridable methods ====== */
-
-        self.loadItem = _abstract;
-    }
-
-
 
     function ModuleViewModel() {
         var self = this;
@@ -75,10 +38,7 @@ define([
         self.args = { };
     }
 
-
-
     return {
-        ModuleViewModel: ModuleViewModel,
-        LegacyModuleViewModel : LegacyModuleViewModel
+        ModuleViewModel: ModuleViewModel
     };
 });
