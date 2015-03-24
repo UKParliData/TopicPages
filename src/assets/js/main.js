7a229ceb6b008eea733d50afc44a1e0cdd5040e5
James McKay <mckayj@parliament.uk>
2015-03-24 11:44:02 +0000

    Install jQuery UI.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,31 @@
 require.config({
   shim: {
-    'selectize' : {
+    selectize: {
       deps: [
         'css!../lib/selectize/dist/css/selectize.css',
         'css!../lib/selectize/dist/css/selectize.default.css'
       ]
+    },
+    'jquery-ui': {
+      deps: [
+        'css!../lib/jquery-ui/themes/black-tie/jquery-ui.css'
+      ]
     }
   },
   paths: {
-    jquery: "../lib/jquery/dist/jquery",
-    requirejs: "../lib/requirejs/require",
-    knockout: "../lib/knockout/dist/knockout",
-    selectize: "../lib/selectize/dist/js/selectize",
-    text: "../lib/text/text",
-    toastr: "../lib/toastr/toastr",
-    "es5-shim": "../lib/es5-shim/es5-shim",
-    microplugin: "../lib/microplugin/src/microplugin",
-    sifter: "../lib/sifter/sifter",
-    css: "../lib/require-css/css",
-    "css-builder": "../lib/require-css/css-builder",
-    normalize: "../lib/require-css/normalize"
+    jquery: '../lib/jquery/dist/jquery',
+    requirejs: '../lib/requirejs/require',
+    knockout: '../lib/knockout/dist/knockout',
+    selectize: '../lib/selectize/dist/js/selectize',
+    text: '../lib/text/text',
+    toastr: '../lib/toastr/toastr',
+    'es5-shim': '../lib/es5-shim/es5-shim',
+    microplugin: '../lib/microplugin/src/microplugin',
+    sifter: '../lib/sifter/sifter',
+    css: '../lib/require-css/css',
+    'css-builder': '../lib/require-css/css-builder',
+    normalize: '../lib/require-css/normalize',
+    'jquery-ui': '../lib/jquery-ui/jquery-ui'
   },
   packages: [
 
