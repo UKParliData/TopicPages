2eda0056debf97d2fe51d1bcde51d75ca6d40e12
James McKay <mckayj@parliament.uk>
2015-05-20 11:36:14 +0100

    Use the "proper" way of creating tooltip.
    According to the SVG specification, tooltips are supposed to be created using
    a <title> element. Unfortunately this doesn't seem to work for reasons that
    completely befuddle me, because I've come across jsFiddle samples that work
    just fine here. The <title> attribute works fine in Firefox and Chrome though.
    
    For the time being, add both a <title> element and a title=... attribute.
    We'll sort this one out later.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -190,12 +190,15 @@ define([
 
                 if (options.tooltips) {
                     $(element).find('rect.bar').each(function(ix, el) {
-                        var cls = Array.prototype.map.call(el.classList, function(x) {
-                            return /^graphGroup([0-9]+)/.exec(x);
-                        })
-                        .filter(function(x) { return x !== null; });
-                        cls = parseInt(cls[0][1], 10);
-                        el.setAttribute('title', groups[cls].content);
+                        var cls = /graphGroup([0-9]+)/.exec(el.className.baseVal);
+                        if (cls && cls.length) {
+                            cls = parseInt(cls[1], 10);
+                            var tag = groups[cls].content;
+                            var tooltip = document.createElement('title');
+                            tooltip.appendChild(document.createTextNode(tag));
+                            el.appendChild(tooltip);
+                            el.setAttribute('title', tag);
+                        }
                     });
                 }
             };
